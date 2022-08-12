3f32a4b91141e837968458409411e4f894344781
Alejo Sanchez <alejoh908@gmail.com>
2022-08-11 21:52:02 -0500

    Add Sidebar

---
+++ src/Login/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Login from './login'
+
+
+function index() {
+    return (
+        <div className="flex w-full h-screen bg-gray-800">
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <Login />
+            </div>
+            <div className='hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-700'>
+                <div className="w-40 h-40 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce" />
+                <div className='w-full h-1/2 absolute bottom-0 bg-gray/10 backdrop-blur-lg' />
+            </div>
+
+        </div>
+    )
+}
+
+export default index
